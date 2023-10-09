bc51ada9caa3af4f1cb34e20b25a3977c50d0499
Masterchef842 <robertwilliamlord55@gmail.com>
2023-10-08 19:14:45 -0500

    added basic navbar

---
+++ choreshare/src/components/bottomNav/BottomNav.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './BottomNav.css'
+
+function bottomNav(){
+
+    return (
+        <div id='bottomBar'>
+            <div className='homeProfButton'>Home</div>
+            <div className='homeProfButton'>Profile</div>
+        </div>
+    );
+}
+
+export default bottomNav;
